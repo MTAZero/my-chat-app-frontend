2e9263b00d93784ac78d7d8a9b1e17ffa3e4e964
Bùi Xuân Thuỷ <buithuy3996@gmail.com>
2021-03-02 02:52:40 +0700

    (fix) Sửa 1 chút giao diện

--- src/pages/test-context-page/index.js
+++ src/pages/test-context-page/index.js
@@ -20,78 +20,74 @@ const LoginPage = () => {
                 <div>Use Redux example</div>
                 <CounterControlComponent />
                 <CounterComponent />
+
+                <div
+                    style={{
+                        backgroundColor: theme.currentTheme.background,
+                        color: theme.currentTheme.foreground,
+                        width: 200,
+                        textAlign: 'center',
+                        padding: 10,
+                    }}
+                >
+                    {theme.currentTheme === themes.light
+                        ? 'Light Theme'
+                        : 'Dark Theme'}
+                </div>
+                <a
+                    href="#"
+                    onClick={() => {
+                        let currentTheme = theme.currentTheme;
+                        let nextTheme = themes.light;
+
+                        if (currentTheme === themes.light)
+                            nextTheme = themes.dark;
+
+                        theme.saveTheme(nextTheme);
+                    }}
+                >
+                    Đổi theme
+                </a>
+
+                <div>Ngôn ngữ: {language.currentLanguage}</div>
+                <div
+                    href="#"
+                    // onClick={() => {
+                    //     let currentLanguage = language.currentLanguage;
+                    //     let nextLanguage = languages.vi;
+
+                    //     if (currentLanguage === languages.vi) nextLanguage = languages.en;
+
+                    //     language.saveLanguage(nextLanguage);
+                    // }}
+                >
+                    <span>
+                        <a
+                            href="#"
+                            style={{ marginRight: 5 }}
+                            onClick={() => language.saveLanguage(languages.vi)}
+                        >
+                            {t('vi')}
+                        </a>
+                        <a
+                            href="#"
+                            style={{ margin: 5 }}
+                            onClick={() => language.saveLanguage(languages.en)}
+                        >
+                            {t('en')}
+                        </a>
+                        <a
+                            href="#"
+                            style={{ margin: 5 }}
+                            onClick={() => language.saveLanguage(languages.cn)}
+                        >
+                            {t('cn')}
+                        </a>
+                    </span>
+                </div>
+                <h3>{t('hello')}</h3>
+                <h2>{t('welcometitle')}</h2>
             </div>
-            <div
-                style={{
-                    backgroundColor: theme.currentTheme.background,
-                    color: theme.currentTheme.foreground,
-                    width: 200,
-                    textAlign: 'center',
-                    padding: 10,
-                }}
-            >
-                {
-                    theme.currentTheme === themes.light ? 'Light Theme' : 'Dark Theme'
-                }
-            </div>
-            <a
-                href="#"
-                onClick={() => {
-                    let currentTheme = theme.currentTheme;
-                    let nextTheme = themes.light;
-
-                    if (currentTheme === themes.light) nextTheme = themes.dark;
-
-                    theme.saveTheme(nextTheme);
-                }}
-            >
-                Đổi theme
-            </a>
-
-            <div>Ngôn ngữ: {language.currentLanguage}</div>
-            <div
-                href="#"
-                // onClick={() => {
-                //     let currentLanguage = language.currentLanguage;
-                //     let nextLanguage = languages.vi;
-
-                //     if (currentLanguage === languages.vi) nextLanguage = languages.en;
-
-                //     language.saveLanguage(nextLanguage);
-                // }}
-            >
-                <span>
-                    <a
-                        href="#"
-                        style={{ marginRight: 5 }}
-                        onClick={() => language.saveLanguage(languages.vi)}
-                    >
-                        {
-                            t("vi")
-                        }
-                    </a>
-                    <a
-                        href="#"
-                        style={{ margin: 5 }}
-                        onClick={() => language.saveLanguage(languages.en)}
-                    >
-                        {
-                            t("en")
-                        }
-                    </a>
-                    <a
-                        href="#"
-                        style={{ margin: 5 }}
-                        onClick={() => language.saveLanguage(languages.cn)}
-                    >
-                        {
-                            t("cn")
-                        }
-                    </a>
-                </span>
-            </div>
-            <h3>{t('hello')}</h3>
-            <h2>{t('welcometitle')}</h2>
         </>
     );
 };
